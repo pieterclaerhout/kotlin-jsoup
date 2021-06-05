e39c3ea0fd41e51f8a7e7666544d94d42ad9b0b9
Pieter Claerhout <pieter@yellowduck.be>
2021-06-05 14:20:29 +0200

    Added coroutines and updated gradle

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,47 +1,83 @@
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.sync.withPermit
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.jsoup.Jsoup
+import org.slf4j.LoggerFactory
 import java.io.File
 import java.net.HttpURLConnection
 import java.net.URL
 
-val client = OkHttpClient.Builder().build()
+object Main {
 
-fun downloadFile(url: String) {
+    init {
+        val rootLogger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME) as Logger
+        rootLogger.level = Level.INFO
+    }
 
-    val request = Request.Builder().url(URL(url)).get().build()
+    val log = LoggerFactory.getLogger(Main::class.java)
 
-    val response = client.newCall(request).execute()
-    if (response.code == HttpURLConnection.HTTP_OK) {
+    val client = OkHttpClient.Builder().build()
 
-        val body = response.body?.bytes()
+    fun downloadFile(url: String) {
 
-        val outDir = File("/Users/pclaerhout/Desktop/fre")
-        outDir.mkdirs()
+        val request = Request.Builder().url(URL(url)).get().build()
 
-        val outPath = File(outDir, File(URL(url).path).name)
+        val response = client.newCall(request).execute()
+        if (response.code == HttpURLConnection.HTTP_OK) {
 
-        if (body != null) {
-            println("Saving: ${outPath}")
-            outPath.writeBytes(body)
+            val body = response.body?.bytes()
+
+            val outDir = File("/Users/pclaerhout/Desktop/fre")
+            outDir.mkdirs()
+
+            val outPath = File(outDir, File(URL(url).path).name)
+
+            if (body != null) {
+                log.info("Saving: ${outPath}")
+                outPath.writeBytes(body)
+            }
+
         }
 
     }
 
-}
+    @JvmStatic
+    fun main(args: Array<String>) = runBlocking {
 
-fun main() {
+        val urls = mutableSetOf<String>()
 
-    for (i in 1..6) {
+        for (i in 1..6) {
 
-        val f = File( "src/main/resources/00${i}.html")
+            val f = File("src/main/resources/00${i}.html")
 
-        val doc = Jsoup.parse(f, "utf-8", "https://www.sportograf.com")
+            val doc = Jsoup.parse(f, "utf-8", "https://www.sportograf.com")
 
-        doc.select(".image").forEach {
-            val url = it.attr("src").replace("thumbnail", "preview")
-            downloadFile(url)
+            doc.select(".image").forEach {
+                val url = it.attr("src").replace("thumbnail", "preview")
+                urls.add(url)
+            }
+
         }
+
+        log.info("Downloading ${urls.size} urls")
+        val requestSemaphore = Semaphore(32)
+
+        val futures = urls.map {
+            launch {
+                // Will limit number of concurrent requests to 5
+                requestSemaphore.withPermit {
+                    downloadFile(it)
+                }
+            }
+        }
+
+        futures.joinAll()
 
     }
 
